7ae0dc5df5aa15d7798b8faa8ee60424f997c1b4
RodriKof91 <rodrigonzalez.carp@gmail.com>
2023-11-07 18:08:36 -0300

    SegundoCommit

--- src/components/ComponenteLabel.jsx
+++ src/components/ComponenteLabel.jsx
@@ -1,24 +1,36 @@
-import jsonData from '../datos.json'
-const datos = jsonData;
+import React, { useState } from 'react';
+import jsonData from '../datos.json';
 
-export const ComponenteLabel = ({ nombre ,opcion}) => {
-    const filteredData = jsonData.filter((item) =>
-        item.categoria === nombre);
-    return (
-        <>
+export const ComponenteLabel = ({ nombre, opcion }) => {
+  const [opcionSeleccionada, setOpcionSeleccionada] = useState('');
+  const [factor, setFactor] = useState(null); 
 
-            <label htmlFor="">{opcion}</label>
+  const handleSeleccion = (event) => {
+    const valorSeleccionado = event.target.value;
+    setOpcionSeleccionada(valorSeleccionado);
 
-            <select>
-                <option value="">...</option>
-                {filteredData.map((item) => (
-                    <option key={item.tipo}>
-                        {item.tipo}
-                    </option>
-                ))}
-            </select>
+    const objetoSeleccionado = jsonData.find((item) => item.tipo === valorSeleccionado);
+    if (objetoSeleccionado) {
+      setFactor(objetoSeleccionado.factor);
+    } else {
+      setFactor(null);
+    }
+  };
 
-        </>
+  const filteredData = jsonData.filter((item) => item.categoria === nombre);
 
-    )
-}
+  return (
+    <>
+      <label htmlFor="">{opcion}</label>
+      <select value={opcionSeleccionada} onChange={handleSeleccion}>
+        <option value="">...</option>
+        {filteredData.map((item) => (
+          <option key={item.tipo} value={item.tipo}>
+            {item.tipo}
+          </option>
+        ))}
+      </select>
+      {factor !== null && <p>Factor seleccionado: {factor}</p>}
+    </>
+  );
+};
