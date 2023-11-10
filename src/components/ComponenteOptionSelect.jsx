b2d6d15c955f5c43891a7260dcc447591cd361f2
RodriKof91 <rodrigonzalez.carp@gmail.com>
2023-11-10 10:24:53 -0300

    FechtDate2Ok

--- src/components/ComponenteOptionSelect.jsx
+++ src/components/ComponenteOptionSelect.jsx
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from 'react';
 export const ComponenteOptionSelect = ({ nombre, opcion, onFactorChange }) => {
   const [opcionSeleccionada, setOpcionSeleccionada] = useState('');
   const [jsonData, setJsonData] = useState([]);
-  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
